aff5736bbed0c1f5363db6538296836a77aaf1a1
Olivier Gauthé <olivier.gauthe@irsamc.ups-tlse.fr>
2020-03-25 23:55:47 +0100

    typedef Dimension, working example. Mssing legs_dim init.

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,17 @@
-/*
-#[derive(Debug)]
-struct AbstractTensor {
-  legs: u64,
-  leg_dim: &Vec<u64>,
-}
-
-impl AbstractTensor {
-  pub fn new(legs: u64, leg_dim: &Vec<u64>) {
-    AbstractTensor {
-      legs,
-      leg_dim,
-    }
-  }
-
-  fn size(&self) -> u64 {
-    let mut s = 1u64;
-    for (i, d) in self.leg_dim.iter().enumerate() {
-      if (self.legs << i)%2 as bool {
-        s *= d;
-      }
-    }
-    s
-  }
-}
-*/
-
-
+type Dimension = u64;
 
 #[derive(Debug, Clone)]
 struct TensorNetwork {
-  cpu: u64,
-  mem: u64,
-  contracted: u64,
-  parent: u64,
-  tensors: Vec<u64>,
+  cpu: Dimension,
+  mem: Dimension,
+  contracted: usize,
+  parent: usize,
+  tensors: Vec<usize>,
 }
 
 
 impl TensorNetwork {
-  pub fn new(tensors: Vec<u64>) -> TensorNetwork {
+  pub fn new(tensors: Vec<usize>) -> TensorNetwork {
     TensorNetwork {
       cpu: 0,
       mem: tensors.iter().map(|x| TensorNetwork::measure(*x)).sum(),
@@ -48,8 +21,8 @@ impl TensorNetwork {
     }
   }
 
-  fn measure(tensor: u64) -> u64 {
-    let mut s = 1u64;
+  fn measure(tensor: usize) -> Dimension {
+    let mut s = 1;
     for (i, d) in leg_dim.iter().enumerate() {
       if (tensor >> i)%2 != 0 {
         s *= d;
@@ -69,7 +42,7 @@ impl TensorNetwork {
           child_tensors.remove(i);
           let ti_dot_tj = ti^tj;
           child_tensors.push(ti_dot_tj);
-          let mem = self.tensors.iter().map(|t| TensorNetwork::measure(*t)).sum::<u64>() + TensorNetwork::measure(*ti) + TensorNetwork::measure(*tj) + TensorNetwork::measure(ti_dot_tj);
+          let mem = self.tensors.iter().map(|t| TensorNetwork::measure(*t)).sum::<Dimension>() + TensorNetwork::measure(*ti) + TensorNetwork::measure(*tj) + TensorNetwork::measure(ti_dot_tj);
           let child = TensorNetwork {
             cpu: self.cpu + TensorNetwork::measure(ti|tj),
             mem: std::cmp::max(self.mem,mem),
@@ -85,27 +58,32 @@ impl TensorNetwork {
   }
 }
 
-fn bruteforce_contraction(tn0: TensorNetwork, n_c:u64) -> Vec<u8> {
+fn bruteforce_contraction(tn0: TensorNetwork, n_c:usize) -> (Vec<usize>,Dimension,Dimension) {
   //let n_l = tn0.tensors.iter().map(|t| 64-t.leading_zeros()).max().unwrap();  // total number of leg != number of legs to contract
-  let n = 1usize<<n_c;
+  let n = 1<<n_c;
   let mut vec:Vec<usize> = (0..n).collect();
   vec.sort_by_key(|c| c.count_ones());
-  let mut tn_vec:Vec<TensorNetwork> = vec![TensorNetwork { cpu:u64::max_value(), mem:0, contracted:0, parent:0, tensors: Vec::new() };n];
+  let mut tn_vec:Vec<TensorNetwork> = vec![TensorNetwork { cpu:Dimension::max_value(), mem:0, contracted:0, parent:0, tensors: Vec::new() };n];
   tn_vec[0] = tn0;
   for &n in vec.iter() {
-    if tn_vec[n].cpu < u64::max_value() {   // some states are not reachable
-      for child in tn_vec[n].generate_children() {
-        if child.cpu < tn_vec[child.contracted as usize].cpu {
-          tn_vec[child.contracted as usize] = child.clone();   // need to clone tensors
-        }
+    for child in tn_vec[n].generate_children() {
+      if child.cpu < tn_vec[child.contracted].cpu {
+        tn_vec[child.contracted] = child.clone();   // need to clone tensors
       }
     }
   }
 
-  vec![0,1,2]
+  let mut sequence:Vec<usize> = Vec::new();
+  let mut k = n-1;
+  while k != 0 {
+    sequence.push(tn_vec[k].contracted);
+    k = tn_vec[k].parent;
+  }
+  sequence.reverse();
+  (sequence, tn_vec[n-1].cpu, tn_vec[n-1].mem)
 }
 
-static leg_dim:[u64;8] = [20,20,9,9,20,9,20,9];
+static leg_dim:[Dimension;8] = [20,20,9,9,20,9,20,9];
 fn main() {
   println!("=============   Begin   ===========");
 
@@ -116,22 +94,24 @@ fn main() {
    *   T-3-E- -2
    *   |   |
    *  -3   -4
+   *
+   *  C: 3
+   *  T1: 21
+   *  T2: 74
+   *  E: 172
+   *
+   * Solution: 0 -> (1 or 2) -> 3 -> 15
+   *
    */
+
   let tensors = vec![3,21,74,172];
 
   let tn0 = TensorNetwork::new(tensors);
   println!("{:?}",tn0);
-  let children = tn0.generate_children();
-  println!("{:?}",children);
-  let tn1 = &children[0];
-  let tn2 = &tn1.generate_children()[0];
-  println!("{:?}",tn2);
-  let children3 = tn2.generate_children();
-  println!("{:?}",children3);
-  println!("{}",tn0.contracted);
 
-  let legs = bruteforce_contraction(tn0,4);
-  println!("{:?}", legs);
+  let (sequence,cpu,mem) = bruteforce_contraction(tn0,4);
+  println!("cpu: {}, mem: {}", cpu, mem);
+  println!("order: {:?}", sequence);
 
   println!("===========   Completed   =========");
 }
